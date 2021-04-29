2ea68b649206c42030284f9cba4c5323f253c459
Felipe SH <shfelipe.dev@gmail.com>
2021-04-29 13:31:09 -0300

    Ver. 1.0

---
+++ ExtrStr.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ExtStr_Rgx
+{
+    public class ExtrStr
+    {
+        /// <summary>
+        /// Retorna o item desejado da coleção de valores encontrados que estão intercalados entre duas strings (de 'ini' até o primeiro 'fim' encontrado).
+        /// </summary>
+        /// <param name="texto">Texto onde se deseja realizar a busca.</param>
+        /// <param name="pos">Número do grupo desejado da coleção em ordem crescente.</param>
+        /// <param name="ini">String de início onde se inicia a busca.</param>
+        /// <param name="fim">String até aonde se faz a busca.</param>
+        /// <param name="regexOptions">Modo como se deseja pesquisar a string. Opção padrão é 'IgnoreCase'.</param>
+        /// <returns>Retorna ou não a posição da string encontrada. Caso não encontre nada, será uma string vazia.</returns>
+        public string ExtStr_Pos(string texto, int pos, string ini, string fim, RegexOptions regexOptions)
+        {
+            // Coleção vetorizada que retorna todos os padrões buscados
+            MatchCollection matches = null;
+
+            // Variável que armazenará o valor possível encontrado
+            string valor_mtc = "";
+
+            // Expressão regular que busca conforme os parâmetros passados
+            matches = Regex.Matches(texto, @"(?<=" + ini + @")[\s\S]*?(?=" + fim + @")", regexOptions);
+
+            int i = 1;
+            // Loop que itera entre a coleção formada
+            foreach (Match match in matches)
+            {
+                if (i < pos)
+                {
+                    i++;
+                    continue;
+                }
+                valor_mtc = match.Value;
+                break; // Quando encontrar o primeiro padrão, retorna o seu respectivo valor
+            }
+            return valor_mtc;
+        }
+
+        /// <summary>
+        /// Retorna o primeiro valor intercalado entre duas strings (de 'ini' até o último 'fim' encontrado).
+        /// </summary>
+        /// <param name="texto">Texto onde se deseja realizar a busca.</param>
+        /// <param name="ini">String de início onde se inicia a busca.</param>
+        /// <param name="fim">String até aonde se faz a busca.</param>
+        /// <param name="regexOptions">Modo como se deseja pesquisar a string. Opção padrão é 'IgnoreCase'.</param>
+        /// <returns>Retorna ou não a primeira string encontrada. Caso não encontre, será uma string vazia.</returns>
+        public string ExtStr_Default(string texto, string ini, string fim, RegexOptions regexOptions)
+        {
+            // Coleção vetorizada que retorna todos os padrões buscados
+            MatchCollection matches = null;
+
+            // Variável que armazenará o valor possível encontrado
+            string valor_mtc = "";
+
+            // Expressão regular que busca conforme os parâmetros passados
+            matches = Regex.Matches(texto, @"(?<=" + ini + @")[\s\S]*(?=" + fim + @")", regexOptions);
+
+            // Loop que itera entre a coleção formada
+            foreach (Match match in matches)
+            {
+                valor_mtc = match.Value;
+                break; // Quando encontrar o primeiro padrão, retorna o seu respectivo valor
+            }
+            return valor_mtc;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Projeto de RegEx desenvolvido em C#
+
+**1 -** Este projeto contém uma classe em C# que possui 3 métodos:
+
+* O primeiro método extrai a cadeia de caracteres a partir de uma posição prevista no texto
+
+* O segundo método extrai a cadeia de caracteres de forma mais simples com a primeira posição encontrada no texto
